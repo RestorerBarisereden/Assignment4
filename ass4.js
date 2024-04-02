eeac294d041d09f7374bedb4f46b649ce5687d36
RestorerBarisereden <restorerzorzor@gmail.com>
2024-04-02 23:34:24 +0100

    first push

---
+++ ass4.js
@@ -0,0 +1,7 @@
+let number = 5;
+if (number %5 === 0 && number %11 === 0){
+    console.log(number + "is divisible by 5 & 11");
+}
+else {
+    console.log(number + "is not divisible by 5 & 11");
+}
